1ca3ef55733e454601626a77093b25dfb95effb5
Adam Hammer <adam.hammer@gmail.com>
2011-07-09 17:35:22 -0400

    Finished initial static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Calling This Home Cookbook"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  def logo
+    logo = image_tag("logo2.jpg", :alt => "Calling This Home Cookbook", :class => "round")
+  end
 end
